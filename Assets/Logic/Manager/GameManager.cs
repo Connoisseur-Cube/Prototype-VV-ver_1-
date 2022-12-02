c5fc5e95ced1eabab03e38edf27f6799eaa1118a
Connoisseur-Cube <muh.ismail1909@gmail.com>
2022-12-03 03:26:15 +0500

    UI Start

---
+++ Assets/Logic/Manager/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //Called First
+    void Awake() 
+    {
+        
+    }
+
+    //Called Second
+    void Start()
+    {
+        
+    }
+
+    //Called once per frame
+    void Update()
+    {
+        
+    }
+    //Called every physics update
+    void FixedUpdate()
+    {
+        
+    }
+
+
+}
